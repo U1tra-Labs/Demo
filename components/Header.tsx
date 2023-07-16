e5831c9aa10247ba169fb1e55b35b311f7886b3b
Penrose <penrose@ultra.sx>
2023-07-16 12:53:51 -0400

    Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,16 +4,16 @@ import Gradient from "./Gradient";
 
 const Header = () => {
   return (
-    <section className="min-h-[90%] flex flex-col mt-[15%] items-center justify-center space-y-9">
-      <h1 className="text-[4rem] text-center flex flex-wrap font-bold">
-        Unleashing Digital Assets: <br /> Powering Blockchain Performance
+    <section className="min-h-screen flex flex-col mt-16 items-center justify-center space-y-9">
+    <h1 className="text-4xl md:text-6xl lg:text-7xl text-center leading-[1.5] font-bold">
+      Unleashing Digital Assets: <br className="hidden md:block"/> Powering Blockchain Performance
       </h1>
-      <p className="text-xl text-center w-[50%] font-normal text-gray-500">
+      <p className="text-xl text-center md:w-[50%] font-normal  text-gray-500">
         A multi-chain lending protocol built on Solana. We bring forward
         multi-chain asset classes married w/ collateralized lending in a simple
         fashion.
       </p>
-      <label className="flex min-w-[600px] items-center h-16 space-x-2 button bg-transparent">
+      <label className="flex md:min-w-[600px] items-center h-16 space-x-2 button bg-transparent">
         <input
           type="email"
           placeholder="Enter your email"

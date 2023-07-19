973d3d7ac11ebd4e552a4e9270099bf1cdff26ec
Penrose <penrose@ultra.sx>
2023-07-19 12:55:05 -0400

    favicon update

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,11 +9,9 @@ const Header = () => {
       Unleashing Digital Assets: <br className="hidden md:block"/> Powering Blockchain Performance
       </h1>
       <p className="text-xl text-center md:w-[50%] font-normal  text-gray-500">
-        A multi-chain lending protocol built on Solana. We bring forward
-        multi-chain asset classes married w/ collateralized lending in a simple
-        fashion.
+      Our Mission is to enable, empower and advance the truly decentralized world for more transparent, fair and efficient markets and products
       </p>
-      <label className="flex md:min-w-[600px] items-center h-16 space-x-2 button bg-transparent">
+      <label className="flex md:min-w-[500px] items-center h-16 space-x-2 button bg-transparent">
         <input
           type="email"
           placeholder="Enter your email"
